f81d64248603eb766df158e4765e24b9c430c6fd
Alfredo España <alfespa17@gmail.com>
2023-12-07 16:57:36 -0600

    Create maint.tf

---
+++ sample/maint.tf
@@ -0,0 +1,16 @@
+module "sample" {
+  source  = "app.terraform.io/alfespa17/sample/google"
+  version = "0.4.0"
+  time= "5s"
+}
+
+
+terraform {
+  cloud {
+    organization = "simple"
+    hostname = "8080-azbuilder-terrakube-lgd86vpndpf.ws-us106.gitpod.io"
+    workspaces {
+      name = "samplecloud"
+    }
+  }
+}
