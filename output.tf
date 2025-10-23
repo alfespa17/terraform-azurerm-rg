453e353c9b3cd8df13094eef42e8303916cf214d
Alfredo España <alfespa17@gmail.com>
2025-10-23 10:35:01 -0600

    Delete output variables for resource group
    Remove output definitions for resource group name and location.

--- output.tf
+++ output.tf
@@ -1,9 +1,1 @@
-output "rg_name" {
-  value       = azurerm_resource_group.rg.name
-  description = "Azure Resource Group"
-}
 
-output "rg_location" {
-  value       = azurerm_resource_group.rg.location
-  description = "Azure Region"
-}
